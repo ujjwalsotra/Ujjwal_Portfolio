fc0c954641e52c7b4ce35487f014bc2621de8eb7
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2023-07-30 21:51:20 +0530

    Basic structure of project and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes , Route} from 'react-router-dom'
+import Home from './routes/Home';
+import Project from './routes/Project';
+import About from './routes/About';
+import Contact from './routes/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='Project/' element={<Project/>}/>
+          <Route path='About/' element={<About/>}/>
+          <Route path='Contact/' element={<Contact/>}/>
+        </Routes>
+    </>
   );
 }
 
