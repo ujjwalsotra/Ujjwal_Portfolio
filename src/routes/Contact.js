c758a405db745136225ebea9e82ea9f287f7d3f3
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2023-08-03 00:21:54 +0530

    built contact page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Hero2 from '../components/Hero2';
+import Form from '../components/Form';
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
       <Hero2 heading="Get in touch" value="Job needed 😀"/>
+      <Form/>
       <Footer/>
     </div>
   )
